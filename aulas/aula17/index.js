bd3811a1199fe25c74c1e2dda53238beb75b4c36
quesia128 <quesia06mntr@gmail.com>
2024-09-23 16:53:47 -0300

    merge

---
+++ aulas/aula17/index.js
@@ -0,0 +1,124 @@
+/*const professor = {
+    nome: 'Lucas', 
+    idade: 32,
+    altura: 1.75,
+    email: 'lucas@gmail.com',
+    tarefas:['Dar aula', 'Responder duvidas'],
+    contarPiadas:function(){
+        console.log("É pa vê ou pa comê?")
+    }
+}
+
+console.log(professor.nome)
+console.log(professor.idade)
+console.log(professor.altura)
+console.log(professor["email"])
+
+professor.nome = 'Leo'
+professor["email"] = 'leo@gmail.com'
+
+console.log(professor.nome)
+console.log(professor.idade)
+console.log(professor.altura)
+console.log(professor["email"])
+*/
+
+//////////////////////////////////////////////////////////////////////////
+/*
+const filme = {
+    direcao: 'Rob Cohen',
+    nome: 'Velozes e Furiosos',
+    anoDeLancamento: 2001,
+    elenco: 'Vin Diesel, Paul Walker, Michelle Rodriguez, Jordana Brewster',
+    filmes:function(){
+        console.log("Você já viu esse filme?")
+    }
+}
+
+console.log(filme.nome)
+console.log(filme.direcao)
+console.log(filme.anoDeLancamento)
+console.log(filme.elenco)
+*/
+
+////////////////////////////////////////////////////////////////
+/*
+const pessoa = {
+    nome: 'Quésia',
+    idade: 16,
+    generoMusical: 'trap',
+    comidasPreferidas: ['lasanha', 'sushi', 'açaí' e 'morango'],
+}
+const amigo = {
+    nomeAmigo: 'Nathalia',
+    idadeAmigo: 16
+}
+
+console.log("O nome da pessoa é", pessoa.nome, "e suas comidas preferidas são",pessoa.comidasPreferidas, "seu melhor amigo se chama", amigo.nomeAmigo, "e tem", amigo.idadeAmigo, "anos")
+*/
+////////////////////////////////////////////////////////////////
+/*
+const donoDoPet = {
+    nome: 'Quésia',
+    idade: 16,
+    pet:{
+        nome:'Marie', 
+        raca: 'Vira-lata',
+        idade:4
+    }
+}
+
+console.log(donoDoPet.pet.nome)*/
+
+////////////////////////////////////////////////////////////////
+/*
+const curso ={
+    nome: 'front End Noturno',
+    linguagens: ['JS', 'CSS', 'HTML']
+}
+
+console.log(curso.linguagens[0])
+curso.numeroEstudantes = 50
+curso['numeroProfessores'] = 10
+
+console.log(curso.numeroEstudante)
+console.log(curso.numeroProfessores)*/
+
+///////////////////////////////////////////////////////////////
+/*
+const professores = [
+    {nome:'Lucas',idade:32,modulo:1},
+    {nome:'Leo',idade:29,modulo:2},
+    {nome:'Mika',idade:29,modulo:3}
+]
+
+console.log(professores[0].nome)*/
+
+///////////////////////////////////////////////////////////////
+/*
+const usuario = {
+    nome: 'Prof', 
+    idade: 27,
+    email: 'prof@senac.com.br',
+    cidade: 'São Leopoldo'
+}
+
+const novoUsuario = {
+    ...usuario,
+    nome: 'Lucas',
+    idade: 32,
+    cidade: 'Canoas'
+}
+
+console.log(novoUsuario)
+
+const nomeProfs = ['Lucas', 'Leo', 'Mika']
+
+const novaListaProfs = [...nomeProfs]
+
+console.log(novaListaProfs)*/
+
+///////////////////////////////////////////////////////////////////////////////////
+
+const personagens = ['Tanjirõ', 'Nezuko', 'Inosuke', 'Sakonji', 'Mitsuri']
+
